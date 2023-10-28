cdbc5519230e5639e2eda69e0e2b167b255c4ed3
skwasim777 <wasimsk6061@gmail.com>
2023-10-28 22:44:33 +0530

    application completed

--- src/main/java/com/java/Employee.java
+++ src/main/java/com/java/Employee.java
@@ -1,33 +1,40 @@
 package com.java;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class Employee {
-		private int eId;
-		private String eName;
-		private Address addr;
-		public int geteId() {
-			return eId;
-		}
-		public void seteId(int eId) {
-			this.eId = eId;
-		}
-		public String geteName() {
-			return eName;
-		}
-		public void seteName(String eName) {
-			this.eName = eName;
-		}
-		public Address getAddr() {
-			return addr;
-		}
-		public void setAddr(Address addr) {
-			this.addr = addr;
-		}
-		@Override
-		public String toString() {
-			return "Employee [eId=" + eId + ", eName=" + eName + ", addr=" + addr + "]";
-		}
-		
-		
-		
-		
+	private int eId;
+	private String eName;
+	@Autowired
+	private Address addr;
+
+	public int geteId() {
+		return eId;
+	}
+
+	public void seteId(int eId) {
+		this.eId = eId;
+	}
+
+	public String geteName() {
+		return eName;
+	}
+
+	public void seteName(String eName) {
+		this.eName = eName;
+	}
+
+	public Address getAddr() {
+		return addr;
+	}
+
+	public void setAddr(Address addr) {
+		this.addr = addr;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [eId=" + eId + ", eName=" + eName + ", addr=" + addr + "]";
+	}
+
 }
